2f60ad5f9e50df932c269d16bd881c0ade55c00a
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-10-14 19:34:29 -0400

    convert if/else to ternary operator for storage

--- notes-functions.js
+++ notes-functions.js
@@ -2,11 +2,8 @@
 // Read existing notes from local storage
 const getSavedNotes =  () => {
     const notesJSON = localStorage.getItem('notes')
-        if (notesJSON !== null) {
-            return JSON.parse(notesJSON)
-        } else {
-            return []
-        }
+    return notesJSON !== null ? JSON.parse(notesJSON) : []
+     
     }
     
     
